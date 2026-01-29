2a22867bfc40e573fe8747a32838c2bff9f1a5ea
Joshua Okoronkwo <joshuaokoronkwo54@gmail.com>
2026-01-29 12:30:13 +0100

    Joshua Commit

---
+++ src/Componenets/projects.js
@@ -0,0 +1,41 @@
+import img1 from "../assets/Img1.png";
+import img2 from "../assets/Img2.png";
+import img3 from "../assets/Img3.png";
+import img4 from "../assets/Img4.png";
+import img5 from "../assets/Img5.png";
+import img6 from "../assets/img6.png";
+
+
+
+export const projects = [
+  {
+    title: "Blog Website",
+    image: img1,
+    url: "https://blog-website-two-beryl.vercel.app/",
+  },
+  {
+    title: "Quiz App",
+    image: img2,
+    url: "https://quizz-app-sandy-iota.vercel.app/",
+  },
+  {
+    title: "Weather App",
+    image: img3,
+    url: "https://weather-app-gamma-three-58.vercel.app/",
+  },
+  {
+    title: "To-Do App",
+    image: img4,
+    url: "https://to-do-ap-seven.vercel.app/",
+  },
+  {
+    title: "Login Form",
+    image: img5,
+    url: "https://login-form-omega-vert.vercel.app/",
+  },
+  {
+    title: "Multi Calculator",
+    image: img6,
+    url: "https://calculator-two-drab-61.vercel.app/",
+  },
+];
