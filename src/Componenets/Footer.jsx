03c53daca135104c6e7e53981e4024475f450ea5
Joshua Okoronkwo <joshuaokoronkwo54@gmail.com>
2026-01-29 12:46:55 +0100

    Joshua Commit

--- src/Componenets/Footer.jsx
+++ src/Componenets/Footer.jsx
@@ -1,7 +1,11 @@
-
-
 function Footer() {
-
+  return (
+    <footer>
+      <footer>
+        &copy; {new Date().getFullYear()} <span id="year"></span> Joshua Okoronkwo. All rights reserved.
+      </footer>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
