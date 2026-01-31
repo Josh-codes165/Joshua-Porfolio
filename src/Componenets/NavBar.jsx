9724f2198a236a078e808f61141dca67b96b81de
Joshua Okoronkwo <joshuaokoronkwo54@gmail.com>
2026-02-01 00:36:52 +0100

    Joshua commit

--- src/Componenets/NavBar.jsx
+++ src/Componenets/NavBar.jsx
@@ -1,24 +1,4 @@
-// function NavBar() {
-
-//     return (
-//         <nav>
-//             <h2>Joshua O.</h2>
-//             <ul>
-//                 <li><a href="">Home</a></li>
-//                 <li><a href="">About</a></li>
-//                 <li><a href="">Projects</a></li>
-//                 <li><a href="">Contact</a></li>
-//             </ul>
-//         </nav>
-//     )
-
-// }
-
-// export default NavBar
-
 import { useState } from 'react';
-// import './NavBar.css';
-
 function NavBar() {
     const [isOpen, setIsOpen] = useState(false);
 
